c228a6cecd256b7324081dc8197e581166fa1238
Eszti88ú <eszti.bota@gmail.com>
2018-08-02 20:42:40 +0200

    Another package for today.

---
+++ Week-01/Day-04/defineBasicInfo.ts
@@ -0,0 +1,39 @@
+//Define several things as a variable and print their values
+
+var var1 : number = 10;
+var var2 : number = var1 + 5;
+
+console.log ( var1 );
+console.log ( var2 );
+
+var greeting1 : string = 'Wake up, ';
+var greeting2 : string = greeting1 + 'Neo'
+
+console.log (greeting2);
+
+//My name as a string
+
+let myName : string = 'Eszter Bóta';
+let nameBlaBla : string = 'My name is ';
+
+console.log ( nameBlaBla + myName );
+
+//My age as an integer
+
+let myAge : number = 30;
+let ageBlaBla : string = 'My age in years is ';
+
+console.log ( ageBlaBla + myAge );
+
+//My height in meters as a float
+
+let myHeight : number = 1.70;
+let heightBlaBla : string = 'My height in meter is '
+
+console.log ( heightBlaBla + myHeight );
+
+//Wheter I am married or not as a boolean
+
+let married : boolean = false;
+console.log ( married);
+
