607057b005c8ee07c6083936e5e1ecd3181e4384
Eszti88ú <eszti.bota@gmail.com>
2018-08-04 19:04:47 +0200

    draw-chess-table is completed

---
+++ Week-01/Day-05/draw-chess-table.ts
@@ -0,0 +1,20 @@
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+
+let lineCount: number = 8;
+
+for (let x: number =1; x <= 8; x++) {
+    if (x % 2 == 0) {
+        console.log(' % % % %');        
+    } else {
+        console.log('% % % % ');        
+    }
+}
