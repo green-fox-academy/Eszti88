869f37f29fd36ebf3a4c8f64e387f38a015992fb
Eszti88ú <eszti.bota@gmail.com>
2018-08-04 18:17:44 +0200

    draw-diamond is completed

--- Week-01/Day-05/draw-diamond.ts
+++ Week-01/Day-05/draw-diamond.ts
@@ -12,3 +12,21 @@ let lineCount: number = 7;
 //    *
 //
 // The diamond should have as many lines as lineCount is
+
+for (let x: number = 0; x < lineCount; x++) {
+    if (x == 0) {
+        console.log('   *');       
+    } if (x == 1) {
+        console.log('  ***');        
+    } if (x == 2) {
+        console.log(' *****');        
+    } if (x == 3) {
+        console.log('*******');        
+    } if (x == 4) {
+        console.log(' *****');        
+    } if (x == 5) {
+        console.log('  ***');        
+    } if (x == 6) {
+        console.log('   *');        
+    }
+}
