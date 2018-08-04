a0118ac7219f9efa802547337d136e2e290cc587
Eszti88ú <eszti.bota@gmail.com>
2018-08-04 18:35:10 +0200

    draww-square is completed

---
+++ Week-01/Day-05/draw-square.ts
@@ -0,0 +1,28 @@
+let lineCount: number = 6;
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (let x: number = 0; x < lineCount; x++) {
+    if (x == 0){
+        console.log('%%%%%%');        
+    } if (x == 1) {
+        console.log('%    %');        
+    } if (x == 2) {
+        console.log('%    %');        
+    } if (x == 3) {
+        console.log('%    %');        
+    } if (x == 4) {
+        console.log('%    %');        
+    } if (x == 5) {
+        console.log('%%%%%%');        
+    }
+}
