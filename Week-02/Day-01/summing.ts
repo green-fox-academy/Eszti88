78c960f806b7700d0e8668222afd462d6c2dbd52
Eszti88ú <eszti.bota@gmail.com>
2018-08-06 16:26:08 +0200

    summing.ts has a 2nd version as well as a practice to check myself

--- Week-02/Day-01/summing.ts
+++ Week-02/Day-01/summing.ts
@@ -1,2 +1,20 @@
-Write a function called `sum` that sum all the numbers until the given parameter
+'use strict';
+export { };
+
+// -  Write a function called `sum` that sum all the numbers until 
+//    the given parameter
 // -  The function should return the result
+
+function sum(givenNumber: number) {
+    let myNum: number = 0;
+    for (let i = 0; i < givenNumber; i++) {
+    myNum = myNum + i;
+}
+return myNum;   
+}
+
+console.log(sum(8));
+
+let testNumber: number = 33;
+
+console.log(sum(testNumber));
---
+++ Week-02/Day-01/summing2.ts
@@ -0,0 +1,17 @@
+'use strict';
+export { };
+
+// -  Write a function called `sum` that sum all the numbers 
+//    until the given parameter
+// -  The function should return the result
+
+function sum(givenNumber: number) {
+    let myNum: number = 0;
+    for (let i = 0; i < givenNumber; i++) {
+        myNum = myNum + i;
+    }
+    return myNum;
+}
+
+console.log(sum(5));
+
