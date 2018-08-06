5ef727d732250f0c9bf12d02167dbfbbaa88b88e
Eszti88ú <eszti.bota@gmail.com>
2018-08-06 13:59:23 +0200

    doublings.ts is completed

---
+++ Week-02/Day-01/doubling.ts
@@ -0,0 +1,13 @@
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter 
+//    and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+let baseNum: number = 123;
+
+function doubling(number: number) {
+    number = baseNum * 2;
+    return number;
+}
+
+console.log(doubling(baseNum));
