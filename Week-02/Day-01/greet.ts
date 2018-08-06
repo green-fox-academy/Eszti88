45f78f81a6b64938259dec9662d3d875481e395e
Eszti88ú <eszti.bota@gmail.com>
2018-08-06 14:26:09 +0200

    greet.ts is completed

---
+++ Week-02/Day-01/greet.ts
@@ -0,0 +1,17 @@
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+export{}
+let name: string = 'Greenfox';
+
+let greet = function (greeting: string = 'Visitor!') {
+    /*greeting = 'Greetings, dear' + name;
+    return greeting;*/
+    console.log("Greetings, dear " + greeting);
+}
+
+greet(name);
+greet();
+
