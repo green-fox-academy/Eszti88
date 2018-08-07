9568fe36fef08c9258d3543b98abac9867d32008
Eszti88ú <eszti.bota@gmail.com>
2018-08-07 17:03:59 +0200

    url-fixer.ts is completed

---
+++ Week-02/Day-02/url-fixer.ts
@@ -0,0 +1,11 @@
+'use strict';
+export{};
+
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+
+let urlCorrection: string = url.replace('https//www.reddit.com/r/nevertellmethebots', 'https://www.reddit.com/r/nevertellmetheodds');
+
+console.log(urlCorrection);
