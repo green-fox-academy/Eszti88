d91b9f594d27dded7e17627e2cd66cda0c22336e
Eszti88ú <eszti.bota@gmail.com>
2018-08-08 13:00:56 +0200

    centered-square is completed again, now in a different way

--- Week-02/Day-03/centered-square-ts.ts
+++ Week-02/Day-03/centered-square-ts.ts
@@ -5,6 +5,22 @@ const ctx = canvas.getContext('2d');
 
 // Draw a green 10x10 square to the center of the canvas.
 
+// In a bit difficult way:
+
+function coordinates (x: number, y: number) {
+    let centerX = canvas.width / 2;
+    let centerY = canvas.height / 2;
+    console.log(centerX, centerY); 
+    
+    ctx.fillStyle = "green";
+    ctx.fillRect(centerX - 5, centerY - 5, 10, 10);     
+}
+
+coordinates(20, 20);
+
+
+
+
 /* // The easiest solution, as the width and height of the canvas are known
 
 ctx.fillStyle = "green";
