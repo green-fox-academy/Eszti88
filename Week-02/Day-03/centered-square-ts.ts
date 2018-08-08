c545bd92a8f49cf437e4e3d17bcb983b101bad7d
Eszti88ú <eszti.bota@gmail.com>
2018-08-08 12:54:27 +0200

    centered-square is completed in an easy way

---
+++ Week-02/Day-03/centered-square-ts.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// Draw a green 10x10 square to the center of the canvas.
+
+/* // The easiest solution, as the width and height of the canvas are known
+
+ctx.fillStyle = "green";
+ctx.fillRect(295, 195, 10, 10);
+*/
+
