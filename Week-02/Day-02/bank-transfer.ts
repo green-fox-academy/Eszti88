775f3aa2738e60012de3fcfccd811a7940608ee4
Eszti88ú <eszti.bota@gmail.com>
2018-08-09 21:34:36 +0200

    the first part of bank-transfer.ts is completed

--- Week-02/Day-02/bank-transfer.ts
+++ Week-02/Day-02/bank-transfer.ts
@@ -1,22 +1,23 @@
 'use strict';
+export{};
 
 const accounts: any[] = [
   { clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
   { clientName: 'Vladimir', accountNumber: 43546731, balance: 5204100071.23 },
   { clientName: 'Sergei', accountNumber: 23456311, balance: 1353600.0 }
-];-
+];
 
 // Create function that returns the name and balance of cash on an account in a list
 // getNameAndBalance(11234543);
 // should return: ['Igor', 203004099.2]
 
-function getNameAndBalance (inputList: any[]) {
-    let nameAndBalance: any = 0;
+function getNameAndBalance (inputList: string[]) {
+    let nameAndBalance: string[]= [];
     for (let i: number = 0; i < inputList.length; i++) {
         console.log(inputList[i]["clientName"], inputList[i]["balance"]);        
     }
 }
-console.log(getNameAndBalance(accounts));
+(getNameAndBalance(accounts));
 
 
 // Create function that transfers an amount of cash from one account to another
@@ -34,8 +35,8 @@ console.log(getNameAndBalance(accounts));
 //	{ clientName: 'Vladimir', accountNumber: 43546731, balance: 5204099571.23 },
 //	{ clientName: 'Sergei', accountNumber: 23456311, balance: 1354100.0 }
 //]
-export = {
+/*export = {
   getNameAndBalance,
   transferAmount,
   accounts
-};
+};*/
