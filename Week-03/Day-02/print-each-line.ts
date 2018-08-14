b6c335bb32a3f951cb3c585917fa9db741686448
Eszti88ú <eszti.bota@gmail.com>
2018-08-14 15:25:31 +0200

    print Please enter the commit message for your changes. Lines starting

--- Week-03/Day-02/print-each-line.ts
+++ Week-03/Day-02/print-each-line.ts
@@ -5,3 +5,29 @@ declare function require(path: string): any;
 // each of lines form the file.
 // If the program is unable to read the file (for example it does not exists),
 // then it should print an error message like: 'Unable to read file: my-file.txt'
+
+const fs = require('fs');
+const charEncoding = 'utf-8';
+
+// létrehozom a my-file.txt-t
+
+function writeToAFile(fileName: string, data: any): void {
+    fs.writeFileSync(fileName, data);
+}
+
+writeToAFile('my-file.txt', 'this is the first row');
+
+
+// itt pedig megnyitom és kiíratom a tartalmát
+function opensFile(fileName: string): string {
+    try {
+        if (fileName === null) {
+            throw new Error('Unable to read file: ' + fileName)
+        }
+        return fs.readFileSync(fileName, charEncoding);
+    } catch (e) {
+        return (e.message);
+    }
+}
+
+console.log(opensFile('my-file.txt'));
