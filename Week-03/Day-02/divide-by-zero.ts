8ca7f093ee4796aae8a942d379c23e9684d31d5c
Eszti88ú <eszti.bota@gmail.com>
2018-08-14 14:39:01 +0200

    divide-by-zero.ts is completed, thanks to Jani's explanation

---
+++ Week-03/Day-02/divide-by-zero.ts
@@ -0,0 +1,33 @@
+declare function require(path: string): any;
+'use strict';
+export { };
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+function divideNumber(inputNumber: number): any {
+    try {
+        if (inputNumber === 0) {
+            throw new Error('fail');
+        }
+        let result: number = 10 / inputNumber;
+        return result;
+    } catch (e) {
+        return(e.message);        
+    }
+}
+console.log(divideNumber(0));
+
+
+/* MÚLT PÉNTEKIG ÍGY CSINÁLTAM VOLNA:
+function divideNumber(inputNumber: number): any {
+    if (inputNumber === 0) {
+        return 'fail';      //nem c.log, mert akkor undefinedot is kiír. return kell
+    } else {
+        let result: number = 10 / inputNumber;
+        return result;
+    }    
+}
+console.log(divideNumber(5)); */
---
+++ Week-03/Day-02/keddi-error.ts
@@ -0,0 +1,64 @@
+declare function require(path: string): any;
+'use strict';
+export { };
+
+const fs = require('fs');
+
+const charEncoding = 'utf-8';
+
+//function readFromFile() {
+function readFromFile(fileName: string): string {
+    try {
+        //return fs.readFileSync('hello.txt', charEncoding);
+        return fs.readFileSync(fileName, charEncoding);
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+    // } catch (e) {               // e means error object
+    //console.log(e);
+}
+}
+
+console.log('hello.txt');
+
+
+//console.log(readFromFile());
+/*
+Prints out: 
+Mark egy isten,
+Arnika egy istenno.*/
+
+
+//************************************************************************************************************
+
+function writeToAFile(fileName: string, data: any): void {
+    fs.writeFileSync(fileName, data);
+}
+
+writeToAFile('hello.txt', 'hello coffee');
+
+console.log(readFromFile('hello.txt'));
+
+
+//******************************************************************************************************************
+
+function countChar(char: string): number {
+    let result: number = 0;
+    const fileContent: string = readFromFile('hello.txt');
+    if (fileContent !== null) {
+        //fileContent.split('\r\n')    //   split \r\n-nel széttörjük sorokra a tartalmat
+        //console.log(fileContent);
+        fileContent.split('\r\n').forEach(e => {
+            e.split('').forEach(elem => {
+                if (elem === 'a') {
+                    result++;
+                }
+            })
+        });
+        writeToAFile('result.txt', result)
+        return result;
+    }
+}
+
+console.log(counChar('a'));
