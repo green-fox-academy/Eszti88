ed2f1453a3d66e5fb639afecbe93504cba0144bf
Eszti88ú <eszti.bota@gmail.com>
2018-08-15 20:48:25 +0200

    checkerboard.ts: add: horizontal stripes. Must return to finish.

--- Week-02/Day-03/checkerboard-ts.ts
+++ Week-02/Day-03/checkerboard-ts.ts
@@ -5,12 +5,21 @@ const ctx = canvas.getContext('2d');
 
 // Fill the canvas with a checkerboard pattern.
 
-let squareSize: number = 20;
-let rows: number = canvas.width / squareSize;
-let collumns: number = canvas.height / squareSize;
+let squareSize: number = 15;
+let collumns: number = canvas.width / squareSize;
+let rows: number = canvas.height / squareSize;
+
+for (let i: number = 0; i < collumns; i++) {
+    if (i % 2 == 0) {
+        for (let j: number = 0; j < rows; j++) {
+            ctx.fillRect(j * squareSize, i * collumns, collumns, collumns);   //vízszintes csíkokat ír
+        }
+    }
+}
+
 
 // too complicated and doesn't run :(
-/*for (let i: number = 0; i < rows; i++) { }
+/* for (let i: number = 0; i < rows; i++) { }
     if (rows % 2 == 0) {
         for (let j: number = 0; j < collumns; j++) {
             if (collumns % 2 == 0) {
@@ -22,4 +31,4 @@ let collumns: number = canvas.height / squareSize;
          }
         }
     }
-} */
+}  */
