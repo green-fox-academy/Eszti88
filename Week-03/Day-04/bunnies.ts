099ed89ef6b7c724ea1461e57d02698038bede4e
Eszti88ú <eszti.bota@gmail.com>
2018-08-16 15:00:57 +0200

    bunnies.ts is completed

---
+++ Week-03/Day-04/bunnies.ts
@@ -0,0 +1,13 @@
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively 
+// (without loops or multiplication).
+
+function earCounter(numOfBunnies: number): number {
+    if (numOfBunnies === 1) {
+        return 2;
+    } else {
+        return 2 + earCounter(numOfBunnies - 1);
+    }
+}
+
+console.log(earCounter(4));
