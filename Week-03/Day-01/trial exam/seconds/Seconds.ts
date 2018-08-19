95760ecbc4b5a2e94a2a7b2f54e36f66dc0f2392
Eszti88ú <eszti.bota@gmail.com>
2018-08-19 11:48:38 +0200

    trial exam no.1: seconds.ts is redone by using a function

--- Week-03/Day-01/trial exam/seconds/Seconds.ts
+++ Week-03/Day-01/trial exam/seconds/Seconds.ts
@@ -8,8 +8,14 @@ export{};
 let list: number[] = [1, 2, 3, 4, 5];
 let newList: number[] = [];
 
+/* the way I solved the exercise on the trial exam on the 14th August 2018:
 for (let i: number = 1; i < list.length; i += 2) {
     newList.push(list[i]);
-}
+} */
+
+// the way I solved on the 19th August 2018:
+newList = list.filter(function(value, index, array) {
+    return (index % 2 === 1);
+} );
 
 console.log(newList);
