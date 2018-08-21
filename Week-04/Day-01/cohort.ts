5add845c6161e85898bd01bf1c641ac33bf7a70c
Eszti88ú <eszti.bota@gmail.com>
2018-08-21 16:50:07 +0200

    GreenFox excercise / cohort.ts : not done, must return to complete

---
+++ Week-04/Day-01/cohort.ts
@@ -0,0 +1,11 @@
+'use strict';
+import {Person} from "./person";
+import {Mentor} from "./mentor";
+
+class Cohort {
+    private name: string;
+    private students: string[];
+    private mentors: string[];
+
+    addStudent(Student);
+}
