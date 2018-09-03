4633b605d74bb3996140b373741c2d4fcb3383b4
Eszti88ú <eszti.bota@gmail.com>
2018-09-03 14:33:53 +0200

    add(double.items.ts): double items with forEach

---
+++ Week-02/redo/Day-01/double-items.ts
@@ -0,0 +1,13 @@
+'use strict';
+export{};
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+drinks.forEach(function(elem) {
+  console.log(elem + elem);  
+})
+
