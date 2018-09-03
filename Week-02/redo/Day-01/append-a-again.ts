2da30486954e56982d0953074fe86f76d823482f
Eszti88ú <eszti.bota@gmail.com>
2018-09-03 14:27:54 +0200

    add(append-a-again.ts): letter 'a' is added to elements

---
+++ Week-02/redo/Day-01/append-a-again.ts
@@ -0,0 +1,20 @@
+'use strict';
+export{};
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+//forEach vagy map??
+
+let animals: string[] = ["koal", "pand", "zebr"];
+let newAnimals: string[] = [];
+
+for (let i: number = 0; i < animals.length; i++) {
+  animals[i] += 'a';
+  newAnimals.push(animals[i]);
+}
+
+console.log(newAnimals);
+
+
