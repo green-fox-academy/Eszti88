560129c69fc779a6934a44fd53c9d5691a8bb007
Eszti88ú <eszti.bota@gmail.com>
2018-09-04 07:55:56 +0200

    add(todo-print.ts): todo list is completed

---
+++ Week-02/redo/Day-02/todo-print.ts
@@ -0,0 +1,20 @@
+'use strict';
+export{};
+
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+// Expected outpt:
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+let todoText: string = ' - Buy milk\n';
+let myTodo: string = 'My todo:\n';
+let downloadGames: string = ' - Download games\n';
+let diablo: string = '    - Diablo';
+
+todoText = myTodo + todoText + downloadGames + diablo;
+
+console.log(todoText);
