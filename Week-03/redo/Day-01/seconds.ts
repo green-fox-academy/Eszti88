f26cf1746ce3fd6bffc855b6582a3bce6200bbaa
Eszti88ú <eszti.bota@gmail.com>
2018-09-04 14:43:17 +0200

    add(seconds.ts): a function to return every second element

--- Week-03/redo/Day-01/seconds.ts
+++ Week-03/redo/Day-01/seconds.ts
@@ -5,4 +5,17 @@ export{};
 // and returns a new list with every second element from the orignal list
 // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
 
-console.log(newList);
+let myList: number[] = [1, 2, 3, 4, 5];
+let newList: number[] = [];
+
+function seconds(myList: number[]): number[] {
+  for (let i: number = 0; i < myList.length; i++) {
+    if (myList[i] % 2 === 0) {
+      newList.push(myList[i]);
+    }
+  } 
+  return newList; 
+}
+
+
+console.log(seconds(myList));
