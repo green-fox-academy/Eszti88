21ec5847462276dd826a725ef4e4b967afb872f3
Eszti88ú <eszti.bota@gmail.com>
2018-09-04 10:51:24 +0200

    add(quote-swap.ts): a function to change elements, must return to remove commas

---
+++ Week-02/redo/Day-02/quote-swap.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+// Accidentally I messed up this quote from Richard Feynman.
+// Two words are out of place
+// Your task is to fix it by swapping the right words with code
+// Also, log the sentence to the console with spaces in between.
+// Create a function called quoteSwap().
+
+let words: string[] = ['What', 'I', 'do', 'create,', 'I', 'cannot', 'not', 'understand.'];
+
+// megcserélem a 2. és 5. elemeket a tömbben:
+function quoteSwap(inputArray: string[]): string[] {
+  let change: string = inputArray[5];
+  inputArray[5] = inputArray[2];
+  inputArray[2] = change;
+  return inputArray;
+}
+
+// eltávolítom a vesszőket:
+/* a replace nem működik stringen
+words = words.replace(', ', '');
+words = words.join(' ');*/
+
+console.log(quoteSwap(words));
+
+// Expected output: "What I cannot create I do not understand."
+
+export = quoteSwap;
