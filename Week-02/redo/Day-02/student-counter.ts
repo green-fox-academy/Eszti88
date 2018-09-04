2dcb256ab0bdc5351277ed379b7aa454808ca2c3
Eszti88ú <eszti.bota@gmail.com>
2018-09-04 11:19:45 +0200

    add(student-counter.ts): function that counts all the candies

---
+++ Week-02/redo/Day-02/student-counter.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+const students: any[] = [
+  {name: 'Theodor', age: 3, candies: 2},
+  {name: 'Paul', age: 9.5, candies: 2},
+  {name: 'Mark', age: 12, candies: 5},
+  {name: 'Peter', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'George', age: 10, candies: 1}
+];
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+
+function candyCounter(inputArray: any[]): number {
+  let candies: number = 0;
+  for (let i: number = 0; i < inputArray.length; i++) {
+    candies += inputArray[i]['candies'];
+  }  
+  return candies;
+}
+
+console.log(candyCounter(students));
+
+
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
