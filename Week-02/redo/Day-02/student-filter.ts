329a979c5c54af86fedebe5cbca813af8d9ab650
Eszti88ú <eszti.bota@gmail.com>
2018-09-04 11:46:10 +0200

    add(student-filter): function for more than 4 candies with forEach method

---
+++ Week-02/redo/Day-02/student-filter.ts
@@ -0,0 +1,30 @@
+'use strict';
+export{};
+
+const students: any[] = [
+  { name: 'Mark', age: 9.5, candies: 2 },
+  { name: 'Paul', age: 12, candies: 5 },
+  { name: 'Clark', age: 7, candies: 3 },
+  { name: 'Pierce', age: 12, candies: 7 },
+  { name: 'Sean', age: 10, candies: 1 }
+];
+
+// create a function that takes a list of students and logs:
+// - Who has got more candies than 4 candies
+
+function hasMoreThan4Candies(inputArray: any[]): string[]{
+  let studentList: string[] = [];
+
+  inputArray.forEach(elem => {
+    if (elem['candies'] > 4) {
+      studentList.push(elem['name']);
+    }
+  });
+  return studentList;
+}
+
+console.log(hasMoreThan4Candies(students));
+
+
+// create a function that takes a list of students and logs: 
+//  - how many candies they have on average
