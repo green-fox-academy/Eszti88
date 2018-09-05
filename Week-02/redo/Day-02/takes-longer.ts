9f5559b90ca9f1d66b879ee3196c0d3313643bef
Eszti88ú <eszti.bota@gmail.com>
2018-09-05 19:49:38 +0200

    fix(takes-longer.ts): added a string to a string using substr built-in method

--- Week-02/redo/Day-02/takes-longer.ts
+++ Week-02/redo/Day-02/takes-longer.ts
@@ -5,6 +5,8 @@ export{};
 // Add "always takes longer than" to between the words "It" and "you"
 
 let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
-quote = 'Hofstadter\'s Law: It always takes longer than you expect, even when you take into account Hofstadter\'s Law.'
+let addition: string = 'always takes longer than ';
+
+quote = quote.substr(0, 21) + addition + quote.substr(21);
 
 console.log(quote);
