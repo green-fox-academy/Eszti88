3a198e4295a0e2f8b17370512e9f433f122e1cfc
Eszti88ú <eszti.bota@gmail.com>
2018-09-06 12:03:25 +0200

    add(pokemon): program.ts with a function to choose the best pokemon

---
+++ Week-03/redo/Day-03/pokemon/program.ts
@@ -0,0 +1,29 @@
+
+import { Pokemon } from "./pokemon";
+
+'use strict';
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+function initializePokemon(): Pokemon[] {
+  return [
+    new Pokemon('Bulbasaur', 'leaf', 'water'),
+    new Pokemon('Pikatchu', 'electric', 'water'),
+    new Pokemon('Charizard', 'fire', 'leaf'),
+    new Pokemon('Bulbasaur', 'water', 'fire'),
+    new Pokemon('Kingler', 'water', 'fire')
+  ];
+}
+
+function chooseMe(inputPokemon: Pokemon[]) {
+  let theOne: string = '';
+  for (let i: number = 0; i < inputPokemon.length; i++) {
+    if (inputPokemon[i]['effectiveAgainst'] === 'leaf') {
+      theOne = inputPokemon[i]['name'];
+    }
+  }
+  return theOne;
+}
+
+console.log(chooseMe(pokemonOfAsh) + ', I choose you!');
