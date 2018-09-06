64dc00d892bab0bd7fb0341f745834e785fc4eaa
Eszti88ú <eszti.bota@gmail.com>
2018-09-06 17:35:10 +0200

    add(counter.ts): a class and a function

---
+++ Week-03/redo/Day-03/counter.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+/*Create a Counter class
+which has an integer property
+when creating it should have a default value 0 or we can specify it when creating
+we can add(number) to increase the counter's value by a whole number
+or we can add() without parameters just increasing the counter's value by one
+and we can get() the current value as string
+also we can reset() the value to the initial value*/
+
+class Counter {
+  private integer?: number = 0;
+  
+  constructor(integer: number) {
+    this.integer = integer;
+  }
+
+  public add(inputNumber?: number): number {
+    if (!inputNumber) {
+      return this.integer++;
+    } else {
+      return this.integer += inputNumber;
+    }
+  }
+}
+
+let myNumber = new Counter(5);
+myNumber.add();
+myNumber.add(2);
+console.log(myNumber);
+
+
