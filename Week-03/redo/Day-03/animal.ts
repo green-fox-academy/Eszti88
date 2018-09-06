ec962fd8647244494276ee5b0f2a1c06c49207ff
Eszti88ú <eszti.bota@gmail.com>
2018-09-06 17:12:03 +0200

    add(animal.ts): functions to eat, drink and play

---
+++ Week-03/redo/Day-03/animal.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+/*Create an Animal class
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one*/
+
+class Animal {
+  private hungerValue: number;
+  private thirstValue: number;
+
+  constructor(hungerValue: number = 50, thirstValue: number = 50) {
+    this.hungerValue = hungerValue;
+    this.thirstValue = thirstValue;
+  }
+
+  public eat(): number {
+    return this.hungerValue--;
+  }
+
+  public drink(): number {
+    return this.thirstValue--;
+  }
+
+  public play(): number {
+    return this.hungerValue++ && this.thirstValue++;
+  }
+}
+
+let dog = new Animal;
+dog.drink();
+dog.eat();
+
+console.log(dog);
