ce7ebca246e79fdc76d9753512a6702d90c3f1aa
Eszti88ú <eszti.bota@gmail.com>
2018-09-07 19:42:26 +0200

    fix(garden application): some small fixes

--- Week-04/redo/Day-02/garden-application/program.ts
+++ Week-04/redo/Day-02/garden-application/program.ts
@@ -5,14 +5,14 @@ import { Tree } from "./tree";
 import { Garden } from "./garden";
 
 let flower1 = new Flower('yellow Flower');
-let flower2 = new Flower('blue Flower');
-let tree1 = new Tree('purple Tree');
-let tree2 = new Tree('orange Tree');
+let flower2 = new Flower('Blue Flower');
+let tree1 = new Tree('Purple Tree');
+let tree2 = new Tree('Orange Tree');
 
 let myGarden = new Garden();
 
 myGarden.add(flower1);
-myGarden.add(flower2)
+myGarden.add(flower2);
 myGarden.add(tree1);
 myGarden.add(tree2);
 
