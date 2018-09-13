74ab62101af0b12023e340ebe2199b23808e27c8
Eszti88ú <eszti.bota@gmail.com>
2018-09-13 12:02:22 +0200

    fix(my first static file): some small changes added

--- Week-06/Day-04/index.js
+++ Week-06/Day-04/index.js
@@ -3,16 +3,16 @@ const path = require('path');
 const app = express();              //szeretnék egy express szervert
 const PORT = 3000;                  //nagybetűvel, igen, mert állandónak tekintjük
 
+app.get('/', (req, res) => {
+  //console.log(__dirname);   //alulvonás, alulvonás dirname: mindig elhoz ebbe a mappába, nem kell megadni az útvonalat
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 app.get('/ping', (req, res) => {
   res.json({
     message: 'pong',
     status: 200,
   });
-});
-
-app.get('/', (req, res) => {
-  //console.log(__dirname);   //alulvonás, alulvonás dirname: mindig elhoz ebbe a mappába, nem kell megadni az útvonalat
-  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 /*app.get('/assets/style.css', (req, res) => {
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });*/
 
 //minden statikus fájl esetén, az előző css-es megoldás helyett, sok-sok file esetére:
-app.use('/assets', express.static('assets'))
+app.use('/assets', express.static('assets'));
 
 //console.log('hopsz'); helyett:
 app.listen(PORT, () => {
