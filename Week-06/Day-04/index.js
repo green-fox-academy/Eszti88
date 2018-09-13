e784869b732e4a61c82fbf40b0ee4560f905e622
Eszti88ú <eszti.bota@gmail.com>
2018-09-13 11:59:24 +0200

    feat(my first static file): full nodejs project

--- Week-06/Day-04/index.js
+++ Week-06/Day-04/index.js
@@ -1,10 +1,27 @@
 const express = require('express'); // a node module mappából szeretném az expresst használni
+const path = require('path');
 const app = express();              //szeretnék egy express szervert
 const PORT = 3000;                  //nagybetűvel, igen, mert állandónak tekintjük
 
 app.get('/ping', (req, res) => {
-  res.send('pong');
+  res.json({
+    message: 'pong',
+    status: 200,
+  });
 });
+
+app.get('/', (req, res) => {
+  //console.log(__dirname);   //alulvonás, alulvonás dirname: mindig elhoz ebbe a mappába, nem kell megadni az útvonalat
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+/*app.get('/assets/style.css', (req, res) => {
+  //console.log(__dirname);   //alulvonás, alulvonás dirname: mindig elhoz ebbe a mappába, nem kell megadni az útvonalat
+  res.sendFile(path.join(__dirname, 'assets', 'style.css'));
+});*/
+
+//minden statikus fájl esetén, az előző css-es megoldás helyett, sok-sok file esetére:
+app.use('/assets', express.static('assets'))
 
 //console.log('hopsz'); helyett:
 app.listen(PORT, () => {
